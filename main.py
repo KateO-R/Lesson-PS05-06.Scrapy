5142035243650b15b8e613d41a33c34812c5b8d5
KateO-R <veg2710@gmail.com>
2024-09-28 11:44:18 +0500

    new lesson

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+data = [
+
+    [100, 200, 300],
+    [400, 500, 600],
+    [150, 140, 160]
+
+    ]
+
+list = []
+
+for row in data:
+    for item in row:
+        if item > 190:
+                list.append(item)
+
+print(list)
 
