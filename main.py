65a3dd86ec5ae13e277288a70053f134a2408bdf
KateO-R <veg2710@gmail.com>
2024-09-29 21:43:54 +0500

    hometask code finished

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ url = "https://www.divan.ru/ekaterinburg/category/svet"
 driver.get(url)
 time.sleep(7)
 
-lights = driver.find_elements(By.CLASS_NAME, "div.WdR1o")
+lights = driver.find_elements(By.CLASS_NAME, "_Ud0k")
 
 parsed_data = []
 
 for light in lights:
     try:
-        name = light.find_element(By.CSS_SELECTOR, "span.div.lsooF").text
-        price = light.find_element(By.CSS_SELECTOR, "span.div.q5Uds").text
-        link = light.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        name = light.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+        price = light.find_element(By.CSS_SELECTOR, "span.ui-LD-ZU").text
+        link = light.find_element(By.CSS_SELECTOR, value='a.ui-GPFV8.qUioe.ProductName').get_attribute('href')
     except Exception as e:
         print(f"Mistake by parsing: {e}")
         continue
@@ -31,7 +31,3 @@ with open("lights.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(['Название светильника', 'цена', 'ссылка на товар'])
     writer.writerows(parsed_data)
-
-
-
-
